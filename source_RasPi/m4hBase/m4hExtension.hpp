c71301b75ad130af60d369490c817171e1f755a6
Karl Hartinger <karl@hartingers.at>
2021-08-15 17:33:19 +0200

    RasPi: MQTT base program in C++

---
+++ source_RasPi/m4hBase/m4hExtension.hpp
@@ -0,0 +1,67 @@
+//_____m4hExtension.hpp__________________________khartinger_____
+// g++ m4hMain.cpp m4hBase.cpp -o m4hBase -lmosquitto -lpthread
+// *  This program demonstrates a basic implementation for the
+//    m4hBase template.
+// 1. show which five functions must be provided by the user.
+// 2. print all incoming messages (so you can see that the 
+//    program is working ;))
+// *  All functions of m4hBase are also available.
+//    (For more information see file m4hMain.cpp.)
+// *  m4hMain.cpp must have a line "#include "m4hExtension.hpp"
+// Hardware: (1) Raspberry Pi
+// Updates:
+// 2021-08-15 First release
+// Released into the public domain.
+
+#include "mosquitto.h"            // mosquitto_* functions
+#include "m4hBase.h"              // m4h basic functions
+
+//-------global values------------------------------------------
+extern bool g_prt;                     //true=printf,false=quiet
+void terminate_program(int reason);    // myMqttPeriodicExec
+
+//_______main: print this help text_____________________________
+void f1PrintHelptext()
+{
+ fprintf(stdout, "\nUsage  : m4hBase [-h | -q | pf.conf]\n");
+ fprintf(stdout, "         -h ........ print this help text\n");
+ fprintf(stdout, "         -q ........ no output to stdout, stderr\n");
+ fprintf(stdout, "         pf.conf ... path+filename of config.file (default m4hLocal.conf)\n");
+ fprintf(stdout, "Purpose: (1) Demo for m4hBase\n");
+ fprintf(stdout, "         (2) Program shows all incomming messages.\n");
+ fprintf(stdout, "Author : Karl Hartinger\n");
+ fprintf(stdout, "Version: 2021-08-15");
+ fprintf(stdout, "Needs  : sudo apt-get install libmosquitto-dev\n\n");
+ fprintf(stdout, "Exit program by pressing <ctrl>c\n");
+}
+
+//_______init extension_________________________________________
+// pfConf...path and filename of config file
+void f2Init(std::string pfConf)
+{
+}
+
+//_______react to further mqtt messages_________________________
+void f3OnMessage(struct mosquitto *mosq, 
+ std::string topic, std::string payload)
+{
+ //------print all received messages----------------------------
+ std::cout<<topic<<" | "<<payload<<std::endl;
+}
+
+//_______Possibility for cleanup before end of program__________
+void f4OnExit(struct mosquitto *mosq, int reason)
+{
+}
+
+//_______for periodic actions (a parallel thread)_______________
+void f5Periodic(struct mosquitto *mosq)
+{
+ bool bDoPeriodic=true;                // do "endless"
+ int  iEnd=4;                          // reason for end
+ while(bDoPeriodic) //-----"endless"----------------------------
+ { //...Do something...
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+ };
+ terminate_program(iEnd);
+}
