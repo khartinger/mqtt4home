4e792632e5133b14f74c76858f7b9ef25850dde9
Karl Hartinger <karl@hartingers.at>
2021-08-19 21:28:50 +0200

    C++ demo program for m4hbase

---
+++ source_RasPi/m4hDemo1/C_Demo1.hpp
@@ -0,0 +1,149 @@
+//_____C_Demo1.hpp____________________________khartinger_____
+// g++ m4hMain.cpp m4hBase.cpp -o m4hDemo1 -lmosquitto -lpthread
+// This file contains additional code for m4hExtension.hpp, 
+// so that the file does not become too confusing.
+// * m4hMain.cpp must have a line "#include "m4hExtension.hpp"
+// * m4hExtension.hpp must have a line "#include "C_Demo1.hpp"
+// Hardware: (1) Raspberry Pi
+// Updates:
+// 2021-08-19 First release
+// Released into the public domain.
+
+#include "mosquitto.h"                 // mosquitto_* functions
+#include "m4hBase.h"                   // m4h basic functions
+
+#define DEMO1_SECTION        "demo1"
+#define DEMO1_STARTVALUE_KEY "startvalue"
+#define DEMO1_STARTVALUE     30
+
+//-------global values------------------------------------------
+extern bool g_prt;                     //true=printf,false=quiet
+
+// *************************************************************
+//    class Log: add incomming messages to files
+// *************************************************************
+class Demo1
+{
+ public:
+ //------properties---------------------------------------------
+ std::string pfConfig;                 // path&name config file
+ std::string section;                  // one session msg data
+ protected:
+ //------application specific properties------------------------
+ std::string keys;                     // keys for [demo1]
+ int startvalue;                       // start value
+ public:
+ //------constructor & co---------------------------------------
+ Demo1();                                // constructor
+ void setDefaults();                   // set all default values
+ //------setter and getter methods------------------------------
+ std::string getKeys() {return keys;}  // keys in config file
+ int getStartvalue() { return startvalue; }
+ //------working methods----------------------------------------
+ bool readConfig();                    // read config file
+ bool readConfig(std::string pfConf);  // read config file
+ void show();                          // show config values
+ bool onMessage(struct mosquitto *mosq, std::string topic, std::string payload);
+ void onExit(struct mosquitto *mosq, int reason);
+
+ //-----helper methods------------------------------------------
+};
+
+// *************************************************************
+//       Demo1: constructor & co
+// *************************************************************
+
+//_______Default constructor____________________________________
+Demo1::Demo1() { setDefaults(); }
+
+//_______set all default properties_____________________________
+void Demo1::setDefaults()
+{
+ pfConfig = _CONF_PFILE_;              // path&name config file
+ section  = DEMO1_SECTION;               // prog specifig info
+ keys=std::string(DEMO1_STARTVALUE_KEY);
+ startvalue=DEMO1_STARTVALUE; 
+}
+
+// *************************************************************
+//       Demo1: setter and getter methods
+// *************************************************************
+
+// *************************************************************
+//       Demo1: working methods
+// *************************************************************
+
+//_______read config data (log path) from file__________________
+bool Demo1::readConfig() { return readConfig(pfConfig); }
+
+//_______read config data (log path) from file__________________
+// return: true if log path exist, false on error or not found
+bool Demo1::readConfig(std::string pfConf) 
+{ 
+ if(pfConf.length()<1) return false;   // wrong file name
+ pfConfig=pfConf;                      // remember file name
+ Conf conf=Conf(pfConf);               // config object
+ if(!conf.isReady()) return false;     // config file ready?
+ //------read all lines of section from config file_____________
+ std::vector<std::string> v1;          // all lines of section
+ conf.getSection(section, v1);         // get lines from conf file
+ if(v1.size()<1) return false;         // no valid lines read
+ //------for every line in section------------------------------
+ bool bRet=false;
+ for(int i=0; i<v1.size(); i++) {
+  //-----get key and value--------------------------------------
+  std::string sKey="", sVal="";
+  std::string s1=v1.at(i);
+  if(!conf.split2String(s1, sKey, sVal, ':')) continue;
+  conf.delExtBlank(sKey);
+  conf.delExtBlank(sVal);
+  conf.strToLower(sKey);
+  //-----search key---------------------------------------------
+  if(sKey==DEMO1_STARTVALUE_KEY) {
+   try{ // string to int
+    int temp=std::stoi(sVal);
+    startvalue=temp;
+    bRet=true;
+   } catch(std::invalid_argument) {};
+  }
+ }
+ return bRet;
+}
+
+//_______Show all properties____________________________________
+void Demo1::show()
+{
+ std::cout<<"-----["<<section<<"]------------------------------"<<std::endl;
+ Conf conf=Conf(pfConfig);
+ std::cout<<"config file         | "<<pfConfig;
+ if(!conf.isReady()) std::cout << " (file not found)";
+ std::cout<<std::endl;
+ std::cout<<"all keys            | "<<getKeys()<<std::endl;
+ std::cout<<DEMO1_STARTVALUE_KEY<<"          | "<<startvalue<<std::endl;
+}
+
+//_______act on messages..._____________________________________
+bool Demo1::onMessage(struct mosquitto *mosq, std::string topic, std::string payload)
+{
+ std::cout<<" | -t "<<topic<<" -m "<<payload<<" | "<<std::endl;
+ if(topic=="m4hDemo1/get" && payload=="keys") {
+  std::string sTop="m4hDemo1/ret/keys";
+  std::string sPay=getKeys();
+  bool retain=false;
+  mosquitto_publish(mosq, NULL, sTop.c_str(), sPay.length(), sPay.c_str(), 0, retain);
+ }
+ return true;
+}
+
+//_______Possibility for cleanup before end of program__________
+void Demo1::onExit(struct mosquitto *mosq, int reason)
+{
+ std::cout<<"Thank you for using m4hDemo1!"<<std::endl;
+}
+
+// *************************************************************
+//       Demo1: helper methods
+// *************************************************************
+
+//_______declare a global little helpers object ;)______________
+Demo1 g_demo1=Demo1();
---
+++ source_RasPi/m4hDemo1/m4hExtension.hpp
@@ -0,0 +1,78 @@
+//_____m4hExtension.hpp__________________________khartinger_____
+// g++ m4hMain.cpp m4hBase.cpp -o m4hDemo1 -lmosquitto -lpthread
+// *  This program uses m4hBase to do the following:
+//    ..ToDo..
+// *  All functions of m4hBase are also available.
+//    (For more information see file m4hMain.cpp.)
+// *  m4hMain.cpp must have a line "#include "m4hExtension.hpp"
+// *  m4hExtension.hpp must have a line "#include "C_Demo1.hpp"
+// Hardware: (1) Raspberry Pi
+// Updates:
+// 2021-08-19 First release
+// Released into the public domain.
+
+#include "mosquitto.h"                 // mosquitto_* functions
+#include "m4hBase.h"                   // m4h basic functions
+#include "C_Demo1.hpp"                   // additional code
+
+//-------global values------------------------------------------
+extern bool g_prt;                     //true=printf,false=quiet
+void terminate_program(int reason);
+
+//_______main: print this help text_____________________________
+void f1PrintHelptext()
+{
+ fprintf(stdout, "\nUsage  : m4hDemo1 [-h | -q | pf.conf]\n");
+ fprintf(stdout, "         -h ........ print this help text\n");
+ fprintf(stdout, "         -q ........ no output to stdout, stderr\n");
+ fprintf(stdout, "         pf.conf ... path+filename of config.file (default m4h.conf)\n");
+ fprintf(stdout, "Purpose: ..ToDo..\n");
+ fprintf(stdout, "Author : Karl Hartinger\n");
+ fprintf(stdout, "Version: 2021-08-10");
+ fprintf(stdout, "Needs  : sudo apt-get install libmosquitto-dev\n\n");
+ fprintf(stdout, "Exit program by pressing <ctrl>c\n");
+}
+
+//_______init extension_________________________________________
+// pfConf...path and filename of config file
+void f2Init(std::string pfConf)
+{
+ g_demo1.readConfig(pfConf);            // {a} read config data
+ if(g_prt) g_demo1.show();              // show config values Demo1
+}
+
+//_______react to further mqtt messages_________________________
+void f3OnMessage(struct mosquitto *mosq, 
+ std::string topic, std::string payload)
+{
+ //======Demo1: respond to messages===============================
+ g_demo1.onMessage(mosq, topic, payload);
+
+}
+
+//_______Possibility for cleanup before end of program__________
+void f4OnExit(struct mosquitto *mosq, int reason)
+{
+ //======Demo1: cleanup before end of program=====================
+ g_demo1.onExit(mosq, reason);
+}
+
+
+//_______for periodic actions (a parallel thread)_______________
+void f5Periodic(struct mosquitto *mosq)
+{
+ bool bDoPeriodic=true;                // do "endles"
+ int  iEnd=4;                          // reason for end
+ int  iSec=g_demo1.getStartvalue();
+ char cSec[16];
+ //------count down iSec and exit program on 0------------------
+ while(iSec>0) //-----"endless"---------------------------------
+ { 
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  sprintf(cSec," %d ",iSec);
+  fputs(cSec, stdout); fflush(stdout);
+  iSec--;
+ };
+ printf(" 0 ");
+ terminate_program(iEnd);
+}
