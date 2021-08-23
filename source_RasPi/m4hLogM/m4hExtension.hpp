a0c6044f43a442b0b4dfe8596b281fa4e764da15
Karl Hartinger <karl@hartingers.at>
2021-08-23 20:02:28 +0200

    append every received MQTT-message to a log-file

---
+++ source_RasPi/m4hLogM/m4hExtension.hpp
@@ -0,0 +1,74 @@
+//_____m4hExtension.hpp__________________________khartinger_____
+// g++ m4hMain.cpp m4hBase.cpp -o m4hLogM -lmosquitto -lpthread
+// *  This program uses m4hBase to do the following:
+//    (1) append every received message to a log-file
+//        filename=topic.yymm.log
+//        topic in filename: replace ' ' by '_' and '/' by '@'
+// *  All functions of m4hBase are also available.
+//    (For more information see file m4hMain.cpp.)
+// *  m4hMain.cpp must have a line "#include "m4hExtension.hpp"
+// *  m4hExtension.hpp must have a line "#include "C_LogM.hpp"
+// Hardware: (1) Raspberry Pi
+// Updates:
+// 2021-08-20 First release
+// Released into the public domain.
+
+#include "mosquitto.h"                 // mosquitto_* functions
+#include "m4hBase.h"                   // m4h basic functions
+#include "C_LogM.hpp"                   // additional code
+
+//-------global values------------------------------------------
+extern bool g_prt;                     //true=printf,false=quiet
+void terminate_program(int reason);
+
+//_______main: print this help text_____________________________
+void f1PrintHelptext()
+{
+ fprintf(stdout, "\nUsage  : m4hLogM [-h | -q | pf.conf]\n");
+ fprintf(stdout, "         -h ........ print this help text\n");
+ fprintf(stdout, "         -q ........ no output to stdout, stderr\n");
+ fprintf(stdout, "         pf.conf ... path+filename of config.file (default m4h.conf)\n");
+ fprintf(stdout, "Purpose: append every received message to a log-file.\n");
+ fprintf(stdout, "Filename(s): topic.yymm.log\n");
+ fprintf(stdout, "         topic in filename: replace ' ' by '_' and '/' by '@'\n");
+ fprintf(stdout, "Author : Karl Hartinger\n");
+ fprintf(stdout, "Version: 2021-08-20");
+ fprintf(stdout, "Needs  : sudo apt-get install libmosquitto-dev\n\n");
+ fprintf(stdout, "Exit program by pressing <ctrl>c\n");
+}
+
+//_______init extension_________________________________________
+// pfConf...path and filename of config file
+void f2Init(std::string pfConf)
+{
+ g_logm.readConfig(pfConf);            // read conf data
+ if(g_prt) g_logm.show();              // show config values LogM
+}
+
+//_______react to further mqtt messages_________________________
+void f3OnMessage(struct mosquitto *mosq, 
+ std::string topic, std::string payload)
+{
+ //======LogM: respond to messages===============================
+ g_logm.onMessage(mosq, topic, payload);
+}
+
+//_______Possibility for cleanup before end of program__________
+void f4OnExit(struct mosquitto *mosq, int reason)
+{
+ //======LogM: cleanup before end of program=====================
+ g_logm.onExit(mosq, reason);
+}
+
+
+//_______for periodic actions (a parallel thread)_______________
+void f5Periodic(struct mosquitto *mosq)
+{
+ bool bDoPeriodic=true;                // do "endles"
+ int  iEnd=4;                          // reason for end
+ while(bDoPeriodic) //-----"endless"----------------------------
+ { //...Do something...
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+ };
+ terminate_program(iEnd);
+}
