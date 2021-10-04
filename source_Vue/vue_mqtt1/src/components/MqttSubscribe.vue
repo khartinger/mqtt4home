f24196d587e957d79d9fb9f72de7fec6b7301e0d
Karl Hartinger <karl@hartingers.at>
2021-10-04 20:08:16 +0200

    Update MqttSubscribe.vue: replace success -> subscribed

--- source_Vue/vue_mqtt1/src/components/MqttSubscribe.vue
+++ source_Vue/vue_mqtt1/src/components/MqttSubscribe.vue
@@ -2,8 +2,8 @@
 <template>
   <div class="mqtt_title">Subscribe topic</div>
   <p>Subscribe topic: <input v-model="topicsub"></p>
-  <button @click="subscribe()"   :disabled="(!isConnected) || (isSuccess)">Subscribe</button> &nbsp;
-  <button @click="unsubscribe()" :disabled="(!isConnected) || (!isSuccess)">Unsubscribe</button> &nbsp;
+  <button @click="subscribe()"   :disabled="(!isConnected) || (isSubscribed)">Subscribe</button> &nbsp;
+  <button @click="unsubscribe()" :disabled="(!isConnected) || (!isSubscribed)">Unsubscribe</button> &nbsp;
 </template>
 
 <script lang="ts">
@@ -13,28 +13,36 @@ import { mqttClientInstance } from '@/services/MqttClientInstance'
 export default defineComponent({
   data () {
     return {
-      topicsub: '#'
+      topicsub: mqttClientInstance.mqttSubscription.topic
     }
   },
   computed: {
+    subscribedTopic: function (): string {
+      return mqttClientInstance.mqttSubscription.topic
+    },
     isConnected: function (): boolean {
       return mqttClientInstance.mqttState.connected
     },
-    isSuccess: function (): boolean {
-      return mqttClientInstance.mqttSubscription.success
+    isSubscribed: function (): boolean {
+      return mqttClientInstance.mqttSubscription.subscribed
     }
   },
   mounted: async function (): Promise<void> {
     // this.connect()
   },
+  watch: {
+    subscribedTopic: function (newValue: string): void {
+      this.topicsub = newValue
+    }
+  },
   methods: {
     subscribe: async function (): Promise<void> {
-      mqttClientInstance.subscribe()
-      mqttClientInstance.mqttSubscription.success = true
+      mqttClientInstance.mqttSubscription.topic = this.topicsub
+      console.log('MqttSubscribe.vue-subscribe: topic=' + mqttClientInstance.mqttSubscription.topic)
+      await mqttClientInstance.subscribe()
     },
     unsubscribe: async function (): Promise<void> {
       mqttClientInstance.unsubscribe()
-      mqttClientInstance.mqttSubscription.success = false
     }
   }
 })
