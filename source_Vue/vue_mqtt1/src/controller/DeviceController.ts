8cfe5dc97b143a5b1efe29d187e5e8a4046ca2cf
Karl Hartinger <karl@hartingers.at>
2021-10-06 21:13:07 +0200

    DeviceController.ts: add retain, qos to method public

--- source_Vue/vue_mqtt1/src/controller/DeviceController.ts
+++ source_Vue/vue_mqtt1/src/controller/DeviceController.ts
@@ -1,5 +1,6 @@
 // _____DeviceController.ts______________________________________
 import { Message, MqttClient } from '@/services/MqttClient'
+import type { QoS } from 'mqtt'
 export abstract class DeviceController {
   protected client: MqttClient | null = null;
 
@@ -7,8 +8,8 @@ export abstract class DeviceController {
     this.client = mqttClient
   }
 
-  protected async publish (topic: string, payload: string): Promise<void> {
-    return this.client?.publish(topic, payload, false)
+  protected async publish (topic: string, payload: string, retain: boolean, qos: QoS): Promise<void> {
+    return this.client?.publish(topic, payload, retain, qos)
   }
 
   public abstract onMessage(message: Message): void;
