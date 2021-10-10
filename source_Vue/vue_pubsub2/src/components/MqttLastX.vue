640ebb4c550fdf212e471ee88c18b215bb8622fc
Karl Hartinger <karl@hartingers.at>
2021-10-10 20:45:39 +0200

    Project Vue + RasPi: Sending and receiving MQTT messages

---
+++ source_Vue/vue_pubsub2/src/components/MqttLastX.vue
@@ -0,0 +1,49 @@
+<!-- MqttLastX.vue -->
+<template>
+  <div><h2>Last messages received</h2></div>
+  <div v-if="isSubscribed">
+    <p>
+      Connected to {{ urlSubscribed }}, topic {{ subTopic }}
+    </p>
+    <table>
+      <tr><th>Topic</th><th>Payload</th><th>Retain</th><th>QoS</th></tr>
+      <tr v-for="(message, key) in messages" :key="key">
+        <td class="nobr">{{ message.topic }}</td>
+        <td>{{ message.payload }}</td>
+        <td class="w10">{{ message.retain }}</td>
+        <td class="w10">{{ message.qos }}</td>
+      </tr>
+    </table>
+  </div>
+  <div v-else>
+    Not connected or nothing subscribed :(
+  </div>
+</template>
+
+<script setup lang="ts">
+import { messages } from '@/store/MessageStore'
+import { mqttLastXController } from '@/controller/MqttLastXController'
+const isSubscribed = mqttLastXController.isSubscribe()
+const urlSubscribed = mqttLastXController.urlSubscribe()
+const subTopic = mqttLastXController.subTopic()
+</script>
+
+<style scoped>
+  table {
+    table-layout: fixed;
+    border: 1px solid lightgrey;
+    border-collapse: collapse;
+  }
+  th, td {
+    border: 1px solid lightgrey;
+    overflow-wrap: anywhere;
+    padding: .3em .3em;
+  }
+  td.nobr {
+    white-space: nowrap;
+  }
+.w10 {
+    text-align: center;
+    white-space: nowrap;
+  }
+</style>
