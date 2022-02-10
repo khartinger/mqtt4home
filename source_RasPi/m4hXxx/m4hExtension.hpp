7e16def14e1866d3b2eb8fb1598ed1d0bb8633a0
Karl Hartinger <karl@hartingers.at>
2022-02-10 17:37:29 +0100

    Update base files

--- source_RasPi/m4hXxx/m4hExtension.hpp
+++ source_RasPi/m4hXxx/m4hExtension.hpp
@@ -9,15 +9,16 @@
 // Hardware: (1) Raspberry Pi
 // Updates:
 // 2021-08-19 First release
+// 2022-02-10 add comments, zsec
 // Released into the public domain.
 
-#include "mosquitto.h"                 // mosquitto_* functions
-#include "m4hBase.h"                   // m4h basic functions
+#include "mosquitto.h"            // mosquitto_* functions
+#include "m4hBase.h"              // m4h basic functions
 #include "C_Xxx.hpp"                   // additional code
 
 //-------global values------------------------------------------
 extern bool g_prt;                     //true=printf,false=quiet
-void terminate_program(int reason);
+void terminate_program(int reason);    // myMqttPeriodicExec
 
 //_______main: print this help text_____________________________
 void f1PrintHelptext()
@@ -35,10 +36,13 @@ void f1PrintHelptext()
 
 //_______init extension_________________________________________
 // pfConf...path and filename of config file
-void f2Init(std::string pfConf)
+bool f2Init(std::string pfConf)
 {
- g_xxx.readConfig(pfConf);            // read conf data
+ bool bRet=true;
+ //...Add "g_xxx.readConfig(pfConf);" lines here ;) ...
+ bRet&=g_xxx.readConfig(pfConf);      // read conf data
  if(g_prt) g_xxx.show();              // show config values Xxx
+ return bRet;
 }
 
 //_______react to further mqtt messages_________________________
@@ -47,25 +51,29 @@ void f3OnMessage(struct mosquitto *mosq,
 {
  //======Xxx: respond to messages===============================
  g_xxx.onMessage(mosq, topic, payload);
-
 }
 
 //_______Possibility for cleanup before end of program__________
 void f4OnExit(struct mosquitto *mosq, int reason)
 {
- //======Xxx: cleanup before end of program=====================
  g_xxx.onExit(mosq, reason);
 }
-
 
 //_______for periodic actions (a parallel thread)_______________
 void f5Periodic(struct mosquitto *mosq)
 {
- bool bDoPeriodic=true;                // do "endles"
+ bool bDoPeriodic=true;                // do "endless"
  int  iEnd=4;                          // reason for end
- while(bDoPeriodic) //-----"endless"----------------------------
- { //...Do something...
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
- };
+ int  zsec=600;                        // 60 secs
+ //======"endless loop"=========================================
+ while(bDoPeriodic)
+ {
+  //...Add here the code that should be executed periodically...
+  //...Add "g_xxx.periodic(mosq);" lines here...
+  //
+  //=====wait for 0.1sec========================================
+  if((--zsec)<=0) zsec=600;
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+ }; // end of "endless loop"
  terminate_program(iEnd);
 }
