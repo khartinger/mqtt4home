1db9fc4a92ca25c9ad3c079c289ef54f6db15abf
Karl Hartinger <karl@hartingers.at>
2023-06-06 07:33:13 +0200

    Delete old version

--- source_Vue/vue66_ci_mqtt_door1/src/services/CiMqttClientInstance.ts
+++
@@ -1,7 +0,0 @@
-// ______CiMqttClientInstance.ts_________________khartinger_____
-// 2023-06-04 New
-import { CiMqttClient } from './CiMqttClient'
-import { ciDoor1Controller } from '@/controller/CiDoor1Controller'
-
-export const ciMqttClientInstance = new CiMqttClient(true)
-ciMqttClientInstance.registerController(ciDoor1Controller)
