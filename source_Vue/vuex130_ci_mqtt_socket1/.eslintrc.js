d55f54c5678c27b0d3c920bb1beb5fba238cc802
Karl Hartinger <karl@hartingers.at>
2024-08-13 16:36:34 +0200

    show symbol socket1

--- source_Vue/vuex130_ci_mqtt_socket1/.eslintrc.js
+++ source_Vue/vuex130_ci_mqtt_socket1/.eslintrc.js
@@ -1,3 +1,4 @@
+// ______.eslintrc.js____________________________2023-01-02_____
 module.exports = {
   root: true,
   env: {
@@ -9,13 +10,18 @@ module.exports = {
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    project: './tsconfig.json',
   },
   rules: {
+    'no-void': 'off',
+    '@typescript-eslint/no-floating-promises': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-multi-spaces': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'comma-dangle': ["error", "always-multiline"]
+  },
+  "ignorePatterns": ['vue.config.js']
 }
